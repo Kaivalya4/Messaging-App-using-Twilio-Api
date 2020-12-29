e3d0c70e992527563c3ec67fd29a0d68a72af12c
Kaivalyamani Tripathi <60978175+Kaivalya4@users.noreply.github.com>
2020-12-29 12:47:47 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = "ACf11748bff64da8a5ce7df3a7971bd14d"
-auth_token = "1053a5be00fb08fe2243d5e68c220749"
+account_sid = "************************************"
+auth_token = "*******************"
 my_cell = "+918674990449"
 my_twilio = "+14793484407"
